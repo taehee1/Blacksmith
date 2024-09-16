b4add0a09638cc4879c586ab7ce73c290ac8a377
taehee1 <kt1430@naver.com>
2024-09-17 01:11:13 +0900

    메뉴UI토글

--- Assets/2_Scripts/UiManager.cs
+++ Assets/2_Scripts/UiManager.cs
@@ -6,25 +6,64 @@ using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
+    [Header("무기UI")]
     public WeaponManager weaponManager;
-
+    [Space(10)]
     public Image image;
     public TextMeshProUGUI index;
     public TextMeshProUGUI name;
     public TextMeshProUGUI chance;
     public TextMeshProUGUI damage;
 
+    [Header("메뉴UI")]
+    public GameObject menuUI;
+
+    private bool isMenuOpen;
+
+    [Header("설정UI")]
+    public GameObject settingUI;
+
+    private bool isSettingOpen;
+
     private void Update()
     {
-        UIUpdate();
+        WeaponUIUpdate();
     }
 
-    public void UIUpdate()
+    public void WeaponUIUpdate()
     {
         image.sprite = weaponManager.weaponImage;
         index.text = $"{weaponManager.weaponIndex} 강";
         name.text = $"{weaponManager.weaponName}";
         chance.text = $"성공확률 : {weaponManager.nextWeaponChance}%";
         damage.text = $"데미지 : {weaponManager.weaponDamage}";
+    }
+
+    public void MenuUI()
+    {
+        if (isMenuOpen)
+        {
+            menuUI.SetActive(!isMenuOpen);
+            isMenuOpen = false;
+        }
+        else
+        {
+            menuUI.SetActive(!isMenuOpen);
+            isMenuOpen = true;
+        }
+    }
+
+    public void SettingUI()
+    {
+        if (isSettingOpen)
+        {
+            settingUI.SetActive(!isSettingOpen);
+            isSettingOpen = false;
+        }
+        else
+        {
+            settingUI.SetActive(!isSettingOpen);
+            isSettingOpen = true;
+        }
     }
 }
---
+++ Assets/2_Scripts/_SceneManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _SceneManager : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void InGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
